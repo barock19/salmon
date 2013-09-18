afdc485bf300779708dc429de5286c90dc1db40b
Zidni Mubarock <zidmubarock@gmail.com>
2013-09-18 09:06:52 +0700

    pg compatiblelity

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 gem 'rails', '4.0.0'
-gem 'sqlite3'
+
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -12,3 +12,7 @@ group :doc do
   gem 'sdoc', require: false
 end
 gem 'paperclip'
+group :development do
+	gem 'sqlite3'
+end
+gem 'pg'
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = 'rXDbzk0uWo7FexnbsIUqQ'
+  config.consumer_secret = '66JsIEJ5aocLPUdzibsoupZgvqO5YmXAEaW045ILAuA'
+  config.oauth_token = '711880830-YOHy53yT5nDTrbVOZYhAxacQjQXLpf1ePS9F1fUf'
+  config.oauth_token_secret = 'UmOp0TFZhDw2OIcdjVgwLh1f1imHk6l3IHXFJo9D4'
+end
